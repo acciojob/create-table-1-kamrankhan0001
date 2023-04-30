6769d54f98875af4da888e124b7f2e5b2a02fa29
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-30 12:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function insert_Row() {
   var row = table.insertRow(0);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.innerHTML = "Row1 cell1";
-  cell2.innerHTML = "Row1 cell2";
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
   
 }
